74c272c5a85ceccd1f2fcc1089150bf80e74d208
smith <bisongsmith18@gmail.com>
2025-11-20 18:57:04 +0100

    implementing while loop for my guessing game

--- script.sh
+++ script.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-x=$(( 6 + RANDOM % (7 - 0 + 1) ))
-echo "guess a number from 0 to 14:"
-read -r -p "Enter the value you guest: " y
-if [[ $y -eq $x ]]; then
-echo "you win"
-elif [[ $y -lt $x ]]; then
-echo "you lose"
-echo "you above the win number"
-elif [[ $y -gt $x ]]; then
-echo "you lose"
-echo "you above the win number"
-else
-echo "Invalid number, not within the range"
-fi
-echo "the win answer is $x "
+x=$(( RANDOM % 15 ))
+y=-1
+echo "Guess a number from 0 to 14:"
+while [[ $y -ne $x ]]; do
+    read -r -p "Enter your guess: " y
+    if ! [[ $y =~ ^[0-9]+$ ]]; then
+        echo "Invalid input! Please enter a number."
+        continue
+    fi
+    if [[ $y -eq $x ]]; then
+        echo "Congratulations! You guessed the number."
+        break
+    elif [[ $y -lt $x ]]; then
+        echo "Too low! Try again."
+    else
+        echo "Too high! Try again."
+    fi
+done
+echo "The winning number was $x"
