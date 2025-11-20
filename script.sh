5f7cddc21a43565c6f4774d80ffd306faaec16e4
smith <bisongsmith18@gmail.com>
2025-11-20 17:04:55 +0100

    adding the script in the file

--- script.sh
+++ script.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+x=$(( 6 + RANDOM % (7 - 0 + 1) ))
+echo "guess a number from 0 to 14:"
+read -p "Enter the value you guest: " y
+if [[ $y -eq $x ]]; then
+echo "you win"
+elif [[ $y -lt $x ]]; then
+echo "you lose"
+echo "you above the win number"
+elif [[ $y -gt $x ]]; then
+echo "you lose"
+echo "you above the win number"
+else
+echo "Invalid number, not within the range"
+fi
+echo "the win answer is $x "
